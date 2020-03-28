09c9360adf774f0048781ce0df230213df46b295
Anna <anabrikn@gmail.com>
2020-03-29 02:56:44 +0300

    [#249818]

--- chapter_001/src/test/java/ru/job4j/tdd/template/GeneratorTest.java
+++ chapter_001/src/test/java/ru/job4j/tdd/template/GeneratorTest.java
@@ -16,47 +16,13 @@ public class GeneratorTest {
         assertTrue(result.equals(exp));
     }
 
-    @Test
-    public void outputIfAllFalling() throws RuntimeException {
+    @Test (expected = RuntimeException.class)
+    public void outputIfAllFalling() {
         Generator generator = new PhraseGenerator();
         String pattern = "I am a ${name}, Who are ${subject}? ";
         Map<String, Object> map = Map.of("Kitty", "you");
-        try {
-            String result = generator.produce(pattern, map);
-            fail();
-        } catch (RuntimeException e) {
-            assertNotEquals("", e.getMessage());
-        }
+        String result = generator.produce(pattern, map);
+        String exp = "I am a Kitty, Who are you? ";
+        assertTrue(result.equals(exp));
     }
-/*
-    @Test
-public void thisShouldFailIfExceptionCaught() {
-    //Given...
-    try {
-        // When...
-    } catch (Exception e) {
-        Assert.fail();
-    }
-    // Then...
-}
-
-@Test
-public void thisShouldPassOnlyIfTheExceptionIsCaught() {
-    //Given...
-    try {
-        // When...
-        Assert.fail();
-    } catch (Exception expected) {}
-    // No "then" needed, the fact that it didn't fail is enough.
-}
-
-@Test
-public void irrelevantExceptionThatCouldBeThrown() {
-    //Given...
-    try {
-        // When...
-    } catch (Exception e) {}
-    // Then...
-}
-     */
 }
